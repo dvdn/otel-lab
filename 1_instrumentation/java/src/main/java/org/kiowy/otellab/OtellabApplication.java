bcc63964bc6d6db453d913fdd13578f793428132
Jerome Massard <jerome.massard@kiowy.com>
2023-10-02 21:18:57 +0200

    Java stuff

---
+++ 1_instrumentation/java/src/main/java/org/kiowy/otellab/OtellabApplication.java
@@ -0,0 +1,14 @@
+package org.kiowy.otellab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtellabApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello world");
+		SpringApplication.run(OtellabApplication.class, args);
+	}
+
+}
---
+++ 1_instrumentation/java/src/test/java/org/kiowy/otellab/OtellabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kiowy.otellab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtellabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
